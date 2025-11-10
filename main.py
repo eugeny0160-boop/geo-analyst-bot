1b55c26184dbfc4054679ac044364576c9e337cd
eugeny0160-boop <eugeny0160@gmail.com>
2025-11-10 11:36:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,24 @@ async def run_weekly_summary():
     summary = generate_summary(articles.data, "неделя")
     await send_summary_to_channel(summary, CHANNEL_ID)
 
+async def run_monthly_summary():
+    now = datetime.now()
+    start = now - timedelta(weeks=1)
+    articles = get_articles_by_date_range(start.isoformat(), now.isoformat())
+    summary = generate_summary(articles.data, "неделя")
+    await send_summary_to_channel(summary, CHANNEL_ID)
+
+    async def run_half_year_summary():
+    now = datetime.now()
+    start = now - timedelta(weeks=1)
+    articles = get_articles_by_date_range(start.isoformat(), now.isoformat())
+    summary = generate_summary(articles.data, "неделя")
+    await send_summary_to_channel(summary, CHANNEL_ID)
+
+     async def run_yearly_summary():
+    now = datetime.now()
+    start = now - timedelta(weeks=1)
+    articles = get_articles_by_date_range(start.isoformat(), now.isoformat())
+    summary = generate_summary(articles.data, "неделя")
+    await send_summary_to_channel(summary, CHANNEL_ID)
 # Аналогично: run_monthly_summary, run_half_year_summary, run_yearly_summary
